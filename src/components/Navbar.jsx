26fde7b7d01996c25717bd4d38ec486749ee50d0
Nikki Zapuiz <ma.nikki.zapuiz@gmail.com>
2023-07-18 22:08:35 +0800

    fixed img source

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar() {
         <>
             <nav className="navbar navbar-expand-lg mb-5 shadow" style={{height: 150}}>
                 <div className="container-fluid">
-                    <img src="./src/assets/CAT.png" width={130} height={70} alt="cat" className="animate__animated animate__tada animate__infinite ms-5"/>
+                    <img src="/CAT.png" width={130} height={70} alt="cat" className="animate__animated animate__tada animate__infinite ms-5"/>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
